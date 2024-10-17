4f64c0eb873cb65a961541c9bd305df688cbad67
LouisG <94234242+LouisGodfrin@users.noreply.github.com>
2024-10-17 18:06:18 +0200

    Installation of vue-router v4+ (Exercise 5)

---
+++ vue-oauth-microsoft-graph/src/router/index.js
@@ -0,0 +1,24 @@
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '../pages/HomePage.vue';
+import ConversationsIndexPage from '../pages/ConversationsIndexPage.vue'; // Créez ce composant
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage,
+  },
+  {
+    path: '/conversations',
+    name: 'Conversations',
+    component: ConversationsIndexPage, // Affiche une page de placeholder pour le moment
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
